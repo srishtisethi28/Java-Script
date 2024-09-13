9138f9c5d21e1bd1a46015a1ddf1df8061753094
srishtisethi28 <161003456+srishtisethi28@users.noreply.github.com>
2024-09-13 18:15:49 +0000

    02_dattaypes added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "srish.com"
+var accountPassword = "12345"
+accountCity = "Una"
+
+// accountId = 2  // not allowed
+
+console.log(accountId)
+
+accountEmail = "Srishti@gmail.com"
+accountPassword = "5000"
+accountCity = "Sirsa"
+let accountState;
+console.table([accountId, accountEmail, accountPassword , accountCity , accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all js code as newer version however nowadays it is already seted to newer version but it is a goo practice to write this
+
+//alert(2+5) // it has different syntax to run in node we can't run it like this we can run this on browser
+
+//number => range- 2 to the power 53
+//bigint
+//string =""
+//boolean = true/false
+//null => standalone value - empty
+//undefined =>value not defined yet
+//symbol =>unique
+
+//object
+
+console.log(typeof 16)
