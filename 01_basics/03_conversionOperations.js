2970ce7b44ace49da018e7b55e2fa317f9241def
srishtisethi28 <161003456+srishtisethi28@users.noreply.github.com>
2024-09-14 06:54:14 +0000

    03_conversionOperations added

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,66 @@
+/* Test Case 1
+let score ="33"
+
+console.log(typeof score)
+console.log(typeof(score))
+ 
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+consol.log(valueInNumber)
+*/
+
+/* Test Case 2
+let score = "33abc"
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+*/
+
+/* Test Case 3
+let score = null
+console.log(typeof score)
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+*/
+
+/* Test Case 4
+let score = undefined
+console.log(typeof score)
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+*/
+
+/* Test Case 5
+let score = true
+console.log(typeof score)
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+*/
+
+/* Test Case 6
+let loggedIn = 1
+let booleanIsLoggedIn = Boolean(loggedIn)
+console.log(booleanIsLoggedIn)
+*/
+
+/* Test Case 7
+let loggedIn = ""
+let booleanIsLoggedIn = Boolean(loggedIn)
+console.log(booleanIsLoggedIn)
+*/
+
+/* Test case 8
+let loggedIn = "srish"
+let booleanIsLoggedIn = Boolean(loggedIn)
+console.log(booleanIsLoggedIn)
+*/
+
+/* Test Case 9
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
+*/
