959df4f3a52c22f1d9cb223d104f8832d8faf4bd
srishtisethi28 <161003456+srishtisethi28@users.noreply.github.com>
2024-09-23 15:05:44 +0000

    04_objects.js added

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,11 @@
+//Objects using constructor
+
+/* Test case 1
+const tinderUser = new Object();// singleton object
+// condst tinderUser ={} //non singleton object
+
+tinderUser.id="123abc"
+tinderUser.name = "Srish"
+tinderUser.isLoggedIn = false
+console.log(tinderUser)
+*/
