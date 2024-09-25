db3c80461ad56820f7ff5d0fba2d27eb5dde2357
srishtisethi28 <161003456+srishtisethi28@users.noreply.github.com>
2024-09-25 15:30:15 +0000

    04_objects.js more add

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -9,3 +9,80 @@ tinderUser.name = "Srish"
 tinderUser.isLoggedIn = false
 console.log(tinderUser)
 */
+
+/* Test case 2
+//Object Nesting
+const regularUser = {
+    email: "some@gmail.com",
+    fullName:{
+        userFullName:{
+            firstName: "Srish",
+            lastName:'Sethi'
+        }
+    }
+}
+
+console.log(regularUser.fullName);
+console.log(regularUser.fullName.userFullName.lastName);
+*/
+
+
+/*Test Case 3 : Object Combining
+const obj1 ={1:"a",2:"b"}
+const obj2 ={3:"a",4:"b"}
+
+// const obj3 ={ obj1, obj2}
+// console.log(obj3);
+
+// const obj3 = obj1 +obj2
+// console.log(obj3);
+
+// const obj3 = Object.assign(obj1,obj2)
+// console.log(obj3);
+// console.log(obj1);
+
+
+// const obj3 = Object.assign({},obj1,obj2)
+// console.log(obj3);
+// console.log(obj1);
+
+// const obj3= {...obj1, ...obj2}
+// console.log(obj3);
+*/
+
+/*Test case 4: Objects inside array
+//Objects in array used in database
+const users =[
+    {
+        email:"h@gmail.com"
+    },
+    {
+        email:"h@gmail.com"
+    },
+    {
+        email:"h@gmail.com"
+    }
+    
+]
+console.log(users[1].email);
+*/
+
+const tinderUser ={} //non singleton object
+
+tinderUser.id="123abc"
+tinderUser.name = "Srish"
+tinderUser.isLoggedIn = false
+console.log(tinderUser)
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+
+
+
+
+
+
