5ae71955d5377c98191d3cfee37ea36183813b4b
srishtisethi28 <161003456+srishtisethi28@users.noreply.github.com>
2024-10-17 05:18:32 +0000

    04_iife added

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,12 @@
+// function chai(){
+//     console.log('Db connected')
+// }
+// chai()
+
+(function chai(){
+    console.log('Db connected')
+})();
+
+( ()=>{
+    console.log('Db connected arrow')
+})();
